f24612888080397b4aaba59bcca0981f7a2839d2
Lydia Guarino <lydiahguarino@gmail.com>
2018-11-11 23:23:05 -0600

    add some dumb logging

--- src/main/java/edu/texas/social_computing/hospitals/Main.java
+++ src/main/java/edu/texas/social_computing/hospitals/Main.java
@@ -10,13 +10,18 @@ public class Main {
         // load lists of hospital and residents from file
         List<Resident> residents = FileImporter.importResidents(args[0]);
         List<Hospital> hospitals = FileImporter.importHospitals(args[1]);
+        System.out.println("loaded Files");
+        System.out.println("residents: " + Integer.toString(residents.size()));
+        System.out.println("hospitals: " + Integer.toString(hospitals.size()));
 
         // make lookup tables
         HospitalTable hospitalTable = HospitalTable.create(hospitals);
         ResidentTable residentTable = ResidentTable.create(residents);
+        System.out.println("generated tables");
 
         // call HRPP
         HRPP.run(hospitalTable, residentTable);
+        System.out.println("Done");
 
     }
 }
